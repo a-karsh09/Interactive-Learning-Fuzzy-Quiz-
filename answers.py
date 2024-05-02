36fc47940ec5e7113ff029cdad0606020444e938
a-karsh09 <103216336+a-karsh09@users.noreply.github.com>
2024-05-03 02:45:15 +0530

    Add files via upload

---
+++ answers.py
@@ -0,0 +1,39 @@
+answers = [
+    # Question 1
+    ["FUZZY","PERCEPTRON", "FUZZY SET"],
+    # Question 2
+    ["MEMBERSHIP", "RELATION", "ADALINE"],
+    # Question 3
+    ["HEBBIAN",
+     "SECOND",
+     "HIDDEN"],
+    # Question 4
+    ["LENGTH",
+    "WIDE",
+    "WIDTH"],
+    # Question 5
+    ["PROPOGATION",
+     "GRADIENT",
+     "DESCENT"],
+    # Question 6
+    ["PHENOMENON",
+     "MOMENT",
+     "MOMENTUM"],
+    # Question 7
+    ["MCCULLOCHE",
+     "LOFTI ZADEH",
+     "YANN ANDRE"],
+    # Question 8
+    ["RELATION",
+     "LOGIC",
+     "NETWORKS"],
+    # Question 9
+    ["ADAPTABLITIES",
+     "ENVIRONMENT",
+     "CRISP RELATON"],
+    # Question 10
+    ["PERIOD",
+     "FUZZY SET",
+     "EPOCH"]
+]
+
---
+++ questions.py
@@ -0,0 +1,14 @@
+questions = [
+    "It is a Linear Classifier",
+    "Uses Bipolar activations, weights adjustable, trained using Delta rule?",
+    "These layers in ANN are responsible for Non linear mapping of inputs and weights?",
+    "The number of neurons in a particular layer of a neural network",
+    "XOR problem suffers from this phenomenon?",
+    "A sophisticated gradient descent parameter that prevents learning from getting stuck in local minima?",
+    "First ANN was proposed and demonstrated by?",
+    "First ANN simulated these functions",
+    "Important Characteristics of ANN?",
+    "A full training pass over the entire dataset such that each example has been seen once?",
+]
+
+
